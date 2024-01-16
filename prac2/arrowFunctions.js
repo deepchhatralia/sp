167b3b5c0f13c7f270fd6efe89c9e08fec68a9b6
Deep Chhatralia <deepchhatralia77@gmail.com>
2024-01-16 18:08:33 +0530

    Arrow functions practice

---
+++ prac2/arrowFunctions.js
@@ -0,0 +1,23 @@
+// const obj = {
+//     name: "Deep",
+//     change: (name) => {
+//         this.name = name
+//     }
+// }
+
+// obj.change("D")
+// console.log(obj.name)
+
+
+
+
+// function Start() {
+//     this.count = 0;
+
+//     setInterval(() => {
+//         this.count++
+//         console.log(this.count)
+//     }, 1000);
+// }
+
+// const x = new Start()
